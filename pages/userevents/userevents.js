4c5daa5c8553617b4310046fb2804dc6143b273a
Jan Wouter van Dalen <janwouter89@gmail.com>
2020-12-04 18:38:02 +0800

    did some really awesome stuff connecting it all together

--- pages/userevents/userevents.js
+++ pages/userevents/userevents.js
@@ -12,7 +12,43 @@ Page({
    * Lifecycle function--Called when page load
    */
   onLoad: function (options) {
+    let currentUser = wx.getStorageSync('user')
+    this.setData({currentUser: currentUser})
+    let EventType = new wx.BaaS.TableObject("event_types")
+    EventType.find().then(res => {
+      let occasions = res.data.objects[0].event_occasions_objects.map(item => Object.keys(item)[0])
+      let activities = res.data.objects[0].event_activities
+      this.setData({event_types: res.data.objects, occasions: occasions, activities: activities})
+      console.log("activities", res)
+    }, err => {
+      // err
+    })
+    this.getMyEvents()
+    this.getOtherUserEvents()
+  },
 
+  getMyEvents: function () {
+    let currentUser = this.data.currentUser;
+    let event = new wx.BaaS.TableObject("events");
+    let query = new wx.BaaS.Query()
+    
+    query.compare('creator_id', "=", currentUser.id)
+    event.setQuery(query).find().then(res => {
+      let myEvents = res.data.objects
+      this.setData({myEvents: myEvents});
+    })
+  },
+
+  getOtherUserEvents: function () {
+    let currentUser = this.data.currentUser;
+    let event = new wx.BaaS.TableObject("events");
+    let query = new wx.BaaS.Query()
+    
+    query.compare('creator_id', "!=", currentUser.id)
+    event.setQuery(query).find().then(res => {
+      let otherUserEvents = res.data.objects
+      this.setData({otherUserEvents: otherUserEvents});
+    })
   },
 
   /**
@@ -26,7 +62,22 @@ Page({
    * Lifecycle function--Called when page show
    */
   onShow: function () {
+    let event = new wx.BaaS.TableObject("events")
+    event.find().then(res => {
+      console.log("res", res)
+      this.setData({events: res.data.objects})
+    }, err => {
+      // err
+    })
+  },
 
+  goToEvent: function (e) {
+    
+    console.log('goToEvent', e)
+    let id = e.currentTarget.dataset.eventid
+    wx.navigateTo({
+      url: `/pages/show/show?id=${id}`,
+    })
   },
 
   /**
